7260a49e947efba619af986d8137f85a4d705b90
hammasahmed <mianhammasahmed@gmail.com>
2024-11-06 01:51:14 +0500

    works

--- EMS/src/components/VDB_addlistings.jsx
+++ EMS/src/components/VDB_addlistings.jsx
@@ -36,8 +36,56 @@ function VDB_addlistings(prop) {
     "Hollow square",
     "U-shaped"
   ]
- 
 
+  const venueOptions = [
+    "",
+    "sports_arena",
+    "banquet_hall",
+    "marquee",
+    "conference_hall",
+    "hotel"
+  ];
+
+  const ServingType = ['', 'Self-Serving', 'Full-Serving'];
+
+  const City = ["City", "Islamabad", "Ahmed Nager", "Ahmadpur East", "Ali Khan", "Alipur", "Arifwala", "Attock", "Bhera", "Bhalwal",
+    "Bahawalnagar", "Bahawalpur", "Bhakkar", "Burewala", "Chillianwala", "Chakwal", "Chichawatni", "Chiniot", "Chishtian", "Daska",
+    "Darya Khan", "Dera Ghazi", "Dhaular", "Dina", "Dinga", "Dipalpur", "Faisalabad", "Fateh Jhang", "Ghakhar Mandi", "Gojra", "Gujranwala",
+    "Gujrat", "Gujar Khan", "Hafizabad", "Haroonabad", "Hasilpur", "Haveli", "Lakha", "Jalalpur", "Jattan", "Jampur", "Jaranwala", "Jhang", "Jhelum",
+    "Kalabagh", "Karor Lal", "Kasur", "Kamalia", "Kamoke", "Khanewal", "Khanpur", "Kharian", "Khushab", "Kot Adu", "Jauharabad",
+    "Lahore", "Lalamusa", "Layyah", "Liaquat Pur", "Lodhran", "Malakwal", "Mamoori", "Mailsi", "Mandi Bahauddin", "mian Channu",
+    "Mianwali", "Multan", "Murree", "Muridke", "Mianwali Bangla", "Muzaffargarh", "Narowal", "Okara", "Renala Khurd", "Pakpattan",
+    "Pattoki", "Pir Mahal", "Qaimpur", "Qila Didar", "Rabwah", "Raiwind", "Rajanpur", "Rahim Yar", "Rawalpindi", "Sadiqabad",
+    "Safdarabad", "Sahiwal", "Sangla Hill", "Sarai Alamgir", "Sargodha", "Shakargarh", "Sheikhupura", "Sialkot", "Sohawa",
+    "Soianwala", "Siranwali", "Talagang", "Taxila", "Toba Tek", "Vehari", "Wah Cantonment", "Wazirabad", "Badin", "Bhirkan",
+    "Rajo Khanani", "Chak", "Dadu", "Digri", "Diplo", "Dokri", "Ghotki", "Haala", "Hyderabad", "Islamkot", "Jacobabad", "Jamshoro",
+    "Jungshahi", "Kandhkot", "Kandiaro", "Karachi", "Kashmore", "Keti Bandar", "Khairpur", "Kotri", "Larkana", "Matiari", "Mehar",
+    "Mirpur Khas", "Mithani", "Mithi", "Mehrabpur", "Moro", "Nagarparkar", "Naudero", "Naushahro Feroze", "Naushara", "Nawabshah",
+    "Nazimabad", "Qambar", "Qasimabad", "Ranipur", "Ratodero", "Rohri", "Sakrand", "Sanghar", "Shahbandar", "Shahdadkot", "Shahdadpur",
+    "Shahpur Chakar", "Shikarpaur", "Sukkur", "Tangwani", "Tando Adam", "Tando Allahyar", "Tando Muhammad", "Thatta", "Umerkot", "Warah",
+    "Abbottabad", "Adezai", "Alpuri", "Akora Khattak", "Ayubia", "Banda Daud", "Bannu", "Batkhela", "Battagram", "Birote", "Chakdara",
+    "Charsadda", "Chitral", "Daggar", "Dargai", "Darya Khan", "dera Ismail", "Doaba", "Dir", "Drosh", "Hangu", "Haripur", "Karak", "Kohat",
+    "Kulachi", "Lakki Marwat", "Latamber", "Madyan", "Mansehra", "Mardan", "Mastuj", "Mingora", "Nowshera", "Paharpur", "Pabbi",
+    "Peshawar", "Saidu Sharif", "Shorkot", "Shewa Adda", "Swabi", "Swat", "Tangi", "Tank", "Thall", "Timergara", "Tordher", "Awaran",
+    "Barkhan", "Chagai", "Dera Bugti", "Gwadar", "Harnai", "Jafarabad", "Jhal Magsi", "Kacchi", "Kalat", "Kech", "Kharan", "Khuzdar",
+    "Killa Abdullah", "Killa Saifullah", "Kohlu", "Lasbela", "Lehri", "Loralai", "Mastung", "Musakhel", "Nasirabad", "Nushki", "Panjgur",
+    "Pishin valley", "Quetta", "Sherani", "Sibi", "Sohbatpur", "Washuk", "Zhob", "Ziarat"]
+  const cateringOptions = [
+    { value: "", label: "Catering Type" },
+    { value: "buffet", label: "Starters" },
+    { value: "plated", label: "Main Courses" },
+    { value: "familyStyle", label: "Grilled Items" },
+    { value: "plated", label: "Breads" },
+    { value: "", label: "Desserts" },
+    { value: "", label: "Beverages" }
+  ];
+
+
+  const serviceOptions = [
+    { value: "", label: "Select Service" },
+    { value: "venue", label: "Venue" },
+    { value: "catering", label: "Catering" }
+  ];
 
   const [formData, setFormData] = useState({
     name: "",
@@ -93,7 +141,7 @@ function VDB_addlistings(prop) {
       console.error("Error uploading images:", error);
     }
   };
-  
+
   // Function to handle adding a new time slot
   const addTimeSlot = () => {
     if (startTime && endTime) {
@@ -121,13 +169,10 @@ function VDB_addlistings(prop) {
 
   return (
     <div
-      className=" border-2 border-black align-center h-[140vh] "
-      style={{
-        backgroundImage: "url('https://picsum.photos/seed/1/1920/1080')",
-        backgroundSize: "cover",
-      }}
+      className=" bg-slate-300"
+
     >
-     
+
       <div className="w-[90%] sm:w-[60%] md:w-[60%] rounded-xl mt-10 border-[1px] shadow-lg m-auto">
         <div className="w-full mx-auto bg-white p-8 shadow-lg rounded-xl">
           <h2 className="text-xl font-semibold mb-4">Booking Form</h2>
@@ -144,39 +189,103 @@ function VDB_addlistings(prop) {
                   // value={formData.name}
                   name={formData.name}
                   className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
-                  placeholder="Sofia"
+                  placeholder="Beautiful Marquee at my home"
                   onChange={handleChange}
                   required
                 />
               </div>
 
+              <label className="block text-sm font-medium text-gray-700" >
+                Address
+              </label>
+              <input
+                type="text"
+                id="first-name"
+                // value={formData.name}
+                name={formData.name}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+                placeholder="Dhoke Khaby, Chungi No. 2"
+                onChange={handleChange}
+                required
+              />
+
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+            <div className=" gap-4 mb-4 w-[100%]">
 
 
               <select
-                className="border-2 h-10 rounded-xl pl-4"
-                placeholder="Service Type"
-                // value={listing.serviceType}
+                className="border-2 h-10 rounded-xl pl-4 w-[100%]"
                 onChange={handleChange}
               >
-                <option value="">Select Service</option>
-                <option value="venue">Venue</option>
-                <option value="catering">Catering</option>
+                {serviceOptions.map((option, index) => (
+                  <option key={index} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
               </select>
+             
               <select
-                className="border-2 h-10 rounded-xl pl-4"
-                placeholder="Service Type"
-                // value={listing.serviceType}
+                className="border-2 h-10 rounded-xl pl-4 w-[100%] my-[2%]"
                 onChange={handleChange}
               >
-                <option value="">Venue Type</option>
-                <option value="venue">Venue</option>
-                <option value="catering">Catering</option>
+                {venueOptions.map((option, index) => (
+                  <option key={index} value={option}>
+                    {option ? option.replace("_", " ").replace(/(?:^|\s)\S/g, a => a.toUpperCase()) : "Venue Type"}
+                  </option>
+                ))}
               </select>
 
+              <select
+                className="border-2 h-10 rounded-xl pl-4 w-[100%] my-[2%]"
+                onChange={handleChange}
+              >
+                {cateringOptions.map((option, index) => (
+                  <option key={index} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
 
+              <select className="border-2 h-10 rounded-xl pl-4 w-[100%] my-[2%]">
+                {ServingType.map((type, index) => (
+                  <option key={index} value={type}>
+                    {type || 'Select Serving Type'}
+                  </option>
+                ))}
+              </select>
 
+              <select
+                className="border-2 h-10 rounded-xl pl-4 w-[100%]"
+                onChange={handleChange}
+              >
+                {City.map((option, index) => (
+                  <option key={index} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+              <label className="block text-sm font-medium text-gray-700">
+                Price Per Person
+              </label>
+              <input
+                id=""
+                name={formData.parking}
+                // value={formData.parking}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+                onChange={handleChange}
+                type="number"
+              ></input>
+              <label className="block text-sm font-medium text-gray-700">
+                Serving Capacity
+              </label>
+              <input
+                id="Parking"
+                name={formData.parking}
+                // value={formData.parking}
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+                onChange={handleChange}
+                type="number"
+              ></input>
               <div className=" gap-4 mb-4">
                 <div>
                   <label className="block text-sm font-medium text-gray-700">
@@ -355,7 +464,7 @@ function VDB_addlistings(prop) {
                   ))}
                 </div>
 
-               
+
               </div>
 
 
@@ -415,26 +524,26 @@ function VDB_addlistings(prop) {
               </div>
             </div>
             <div className="image-upload-container">
-      <input
-        type="file"
-        multiple
-        accept="image/*"
-        onChange={handleImageChange}
-      />
-      <div className="image-preview">
-        {selectedImages.map((image, index) => (
-          <div key={index} className="image-preview-item">
-            <img
-              src={URL.createObjectURL(image)}
-              alt={`Preview ${index + 1}`}
-              className="preview-thumbnail"
-            />
-            <button onClick={() => handleRemoveImage(index)}>Remove</button>
-          </div>
-        ))}
-      </div>
-      <button onClick={handleUpload}>Upload Images</button>
-    </div>
+              <input
+                type="file"
+                multiple
+                accept="image/*"
+                onChange={handleImageChange}
+              />
+              <div className="image-preview">
+                {selectedImages.map((image, index) => (
+                  <div key={index} className="image-preview-item">
+                    <img
+                      src={URL.createObjectURL(image)}
+                      alt={`Preview ${index + 1}`}
+                      className="preview-thumbnail"
+                    />
+                    <button onClick={() => handleRemoveImage(index)}>Remove</button>
+                  </div>
+                ))}
+              </div>
+              <button onClick={handleUpload}>Upload Images</button>
+            </div>
             <button
               type="submit"
               className=" py-2 px-5 ml-[85%] bg-blue-500 text-white rounded-md hover:bg-blue-600 transition duration-200"
